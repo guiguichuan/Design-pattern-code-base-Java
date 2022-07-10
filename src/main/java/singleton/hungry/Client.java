1aac1aaba77bf43edf881939e3a0caf22465add6
guichuan <2263814606@qq.com>
2022-07-10 14:30:20 +0800

    设计模式--单例模式-饿汉式
    写了单例模式中的饿汉式，提交的是饿汉式单例示例代码

---
+++ src/main/java/singleton/hungry/Client.java
@@ -0,0 +1,15 @@
+package singleton.hungry;
+
+import singleton.lazybones.hungry.Singleton;
+
+public class Client {
+    public static void main(String[] args) {
+        Singleton singletonA = Singleton.getInstance();
+        Singleton singletonB = Singleton.getInstance();
+        if (singletonA == singletonB) {
+            System.out.println("singletonA 与 singletonB 是相同实例");
+        } else {
+            System.out.println("singletonA 与 singletonB 是不同实例");
+        }
+    }
+}
---
+++ src/main/java/singleton/hungry/Singleton.java
@@ -0,0 +1,19 @@
+package singleton.hungry;
+
+/**
+ * 饿汉式就和名字一样很饿
+ * 迫不及待
+ * 没有加锁，执行效率高，一般都会使用这种
+ * 在类装载的时候就进行了实例化
+ * 这个代码和之前的普通版 其实是一样的
+ * */
+public class Singleton {
+
+    private static Singleton instance = new Singleton();
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
