77cf53807a66ad9038c935bb6f52f8b4bd77c70d
guichuan <2263814606@qq.com>
2022-07-10 18:53:25 +0800

    设计模式--单例模式-登记式 补充一个字

--- src/main/java/singleton/register/Singleton.java
+++ src/main/java/singleton/register/Singleton.java
@@ -6,7 +6,7 @@ package singleton.register;
  * 线程安全
  * 这种方法可以达到双检锁一样的功效，但实现更简单
  * 这种方式可以适用于静态域的情况下。双检锁可以再实例域小阳台延迟初始化的时候用
- * 这种方式同样利用了 classloader 机制来保证初始化 instance 时只有一个线
+ * 这种方式同样利用了 classloader 机制来保证初始化 instance 时只有一个线程
  * */
 public class Singleton {
 
